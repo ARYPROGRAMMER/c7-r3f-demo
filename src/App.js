a4f0074ffc322bb7087e656643a8334620e3100c
ARYPROGRAMMER <arya.2023ug1104@iiitranchi.ac.in>
2024-11-09 06:01:35 +0530

    optimisations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Suspense } from "react";
+import React, { Suspense } from 'react';
 import { Canvas } from "@react-three/fiber";
-import "./style.css";
+import { useProgress, Html } from "@react-three/drei";
 import {
   CubeCamera,
   Environment,
@@ -11,14 +11,66 @@ import { Ground } from "./Ground";
 import { Car } from "./Car";
 import { Rings } from "./CircularRings";
 import { Boxes } from "./Box";
+import { FloatingGrid } from "./Grid";
 import { Bloom, ChromaticAberration, EffectComposer } from "@react-three/postprocessing";
 import { BlendFunction } from "postprocessing";
-import { FloatingGrid } from "./Grid";
 
-function CarShow() {
+// Loading screen using Html from drei
+const Loader = () => {
+  const { progress } = useProgress();
+  return (
+    <Html center>
+      <div style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: 'rgba(0,0,0,0.8)',
+        padding: '20px',
+      }}>
+        <div style={{
+          width: '300px',
+          height: '4px',
+          background: '#121212',
+          borderRadius: '10px',
+          overflow: 'hidden',
+          position: 'relative',
+        }}>
+          <div style={{
+            width: `${progress}%`,
+            height: '100%',
+            background: 'linear-gradient(90deg, #00ff87 0%, #60efff 100%)',
+            transition: 'width 0.5s ease',
+            borderRadius: '10px',
+          }} />
+        </div>
+        <div style={{
+          color: 'white',
+          fontSize: '1.2em',
+          fontWeight: '500',
+          marginTop: '1em',
+          fontFamily: 'Inter, sans-serif',
+          textTransform: 'uppercase',
+          letterSpacing: '0.2em',
+        }}>
+          {progress.toFixed(0)}% loaded
+        </div>
+      </div>
+    </Html>
+  );
+};
+
+const CarShow = () => {
   return (
     <>
-      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.5} />
+      <OrbitControls 
+        target={[0, 0.35, 0]} 
+        maxPolarAngle={1.5}
+        enablePan={false}
+        enableZoom={false}
+      />
       <PerspectiveCamera makeDefault fov={60} position={[3, 2, 5]} />
 
       <color args={[0, 0, 0]} attach="background" />
@@ -28,20 +80,25 @@ function CarShow() {
           <>
             <Environment map={texture} />
             <Car />
-            
           </>
         )}
       </CubeCamera>
-     
-      <Rings />
-     
-      <Boxes />
 
-      
-      <FloatingGrid />
+      <Suspense fallback={null}>
+        <Rings />
+      </Suspense>
+
+      <Suspense fallback={null}>
+        <Boxes />
+      </Suspense>
+
+      <Suspense fallback={null}>
+        <FloatingGrid />
+      </Suspense>
+
       <spotLight
         color={[1, 0.45, 0.7]}
-        intensity={4.2} // Slightly increased from 1.5
+        intensity={4.2}
         angle={0.7}
         penumbra={0.7}
         position={[5, 5, 0]}
@@ -51,7 +108,7 @@ function CarShow() {
 
       <spotLight
         color={[0.14, 0.7, 1]}
-        intensity={3.2} // Slightly increased from 2
+        intensity={3.2}
         angle={0.7}
         penumbra={0.7}
         position={[-5, 5, 0]}
@@ -62,37 +119,44 @@ function CarShow() {
       <Ground />
       
       <EffectComposer>
-       
         <Bloom
-        blendFunction={BlendFunction.ADD}
-        intensity={1.3}
-        width={300}
-        height={300}
-        kernelSize={5}
-        luminanceThreshold={0.15}
-        luminanceSmoothing={0.025}
+          blendFunction={BlendFunction.ADD}
+          intensity={1.3}
+          width={300}
+          height={300}
+          kernelSize={5}
+          luminanceThreshold={0.15}
+          luminanceSmoothing={0.025}
         />
-
         <ChromaticAberration
-        blendFunction={BlendFunction.NORMAL}
-        offset={[0.0005,0.0012]}
+          blendFunction={BlendFunction.NORMAL}
+          offset={[0.0005, 0.0012]}
         />
-
       </EffectComposer>
-      
     </>
   );
-}
+};
 
-function App() {
+const App = () => {
   return (
-    <Suspense fallback={null}>
-      <Canvas shadows>
+    <Canvas 
+      shadows 
+      style={{ 
+        width: '100vw', 
+        height: '100vh', 
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        touchAction: 'none'
+      }}
+      camera={{ position: [3, 2, 5], fov: 60 }}
+    >
+      <Suspense fallback={<Loader />}>
         <ambientLight intensity={0.02} />
         <CarShow />
-      </Canvas>
-    </Suspense>
+      </Suspense>
+    </Canvas>
   );
-}
+};
 
 export default App;
